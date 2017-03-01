f44c210ffc73634a2dbc362d4c1d6a5494f1c305
imre_meszesan <imre.meszesan@gmail.com>
2017-03-01 14:28:15 +0100

    added: - QuartettGame constructor - player instances in run()

--- src/quartettgame/QuartettGame.java
+++ src/quartettgame/QuartettGame.java
@@ -1,6 +1,11 @@
 package quartettgame;
 
+import framework.Deck;
+import framework.DeckBuilder;
 import framework.Game;
+import framework.Player;
+
+import java.util.ArrayList;
 
 /**
  * Created by trixi on 2017.02.28..
@@ -10,14 +15,30 @@ public class QuartettGame implements Game
     /* meghatározza a framework.Player -eket
     *  felépíti a játékteret
     */
+    private Deck quartetDeck;
+    private Player[] playerArray;
+
+    public QuartettGame(QuartettDeckBuilder b, int numOfPlayers) {
+        quartetDeck = b.buildDeck();
+        playerArray = new Player[numOfPlayers];
+    }
 
     @Override
     public void run() {
+        QuartettPlayer lasWinner = null;
+        for (int i = 0; i < playerArray.length; i++){
+            playerArray[i] = new QuartettPlayer("Player " + (i+1), new QuartettHand());
+        }
+        /*do {
+            quartetDeck.shuffleCards();
+            dealCards();
+            lasWinner = (QuartettPlayer) playerArray[1];
+            lasWinner.pickAttribute();
 
+
+        }while ();*/
     }
 
-    public QuartettGame(QuartettDeckBuilder b) {
-    }
     public void dealCards() {
 
     }
